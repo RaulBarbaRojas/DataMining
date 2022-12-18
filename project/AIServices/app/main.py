55c4a3806124d516d099a83e06faf179ff6735d3
Raúl Barba Rojas <raul.barba@alu.uclm.es>
2022-12-18 20:29:34 +0100

    Fixed api

--- project/AIServices/app/main.py
+++ project/AIServices/app/main.py
@@ -37,63 +37,6 @@ def handler_predict_optimal_product(product : OptimalProduct):
     logging.debug('[SERVER]: received request for an optimal product prediction')
     logging.debug('[SERVER]: Year: %d',  product.year)
     logging.debug('[SERVER]: Month: %d',  product.month)
-
-
-
-@app.post('/predict-optimal-product-by-ccaa/')
-def handler_predict_optimal_product_ccaa(product : OptimalProductCCAA):
-    ''' API handler for the optimal product prediction for a given CCAA '''
-    logging.debug('[SERVER]: received request for an optimal product prediction ccaa')
-    logging.debug('[SERVER]: Year: %d',  product.year)
-    logging.debug('[SERVER]: Month: %d',  product.month)
-    logging.debug('[SERVER]: CCAA: %s',  product.ccaa)
-
-
-@app.post('/predict-product/')
-def handler_predict_product(product : SpecificProduct):
-    ''' API handler for specific product prediction '''
-    logging.debug('[SERVER]: received request for a specific product prediction')
-    logging.debug('[SERVER]: Year: %d',  product.year)
-    logging.debug('[SERVER]: Month: %d',  product.month)
-    logging.debug('[SERVER]: Product: %s',  product.product)
-
-
-@app.post('/predict-product-by-ccaa/')
-def handler_predict_product_ccaa(product : SpecificProductCCAA):
-    ''' API handler for the specific product prediction for a given CCAA '''
-    logging.debug('[SERVER]: received request for a specific product prediction ccaa')
-    logging.debug('[SERVER]: Year: %d',  product.year)
-    logging.debug('[SERVER]: Month: %d',  product.month)
-    logging.debug('[SERVER]: Product: %s',  product.product)
-    logging.debug('[SERVER]: CCAA: %s',  product.ccaa)
-
-
-def get_joblib_models():
-    '''Loading the label encoder for products and regions.
-        Loading the gradient boosting model'''
-    encoder_regions = joblib.load('home/app/data/encoder_regions.joblib')
-    encoder_products = joblib.load('home/app/data/encoder_products.joblib')
-    gb_reg = joblib.load('home/app/data/gradient_boosting_model.joblib')
-
-    return encoder_regions,encoder_products,gb_reg
-
-def establish_db_connection():
-    '''Creating psycopg2 connection'''
-    with open('home/app/credentials.json', 'r', encoding = "utf8") as file:
-        credentials = json.load(file)
-
-    db_conn = psycopg2.connect(
-    database = AGRICULTURE_DATABASE,
-    user = credentials['username'],
-    password = credentials['password'],
-    host = HOST,
-    port = PORT
-    )
-    return db_conn
-
-@app.get('/best-product/')
-def get_best_product(product : OptimalProduct):
-    '''Returning the best product for given month'''
     db_conn = establish_db_connection()
     _, product_encoder, model = get_joblib_models()
     db_conn.autocommit = True
@@ -108,9 +51,13 @@ def get_best_product(product : OptimalProduct):
     return {'best_product':best}
 
 
-@app.get('/best-product-for-ccaa/')
-def get_best_product_for_ccaa(product : OptimalProductCCAA):
-    '''Returning the best product for given month and region'''
+@app.post('/predict-optimal-product-by-ccaa/')
+def handler_predict_optimal_product_ccaa(product : OptimalProductCCAA):
+    ''' API handler for the optimal product prediction for a given CCAA '''
+    logging.debug('[SERVER]: received request for an optimal product prediction ccaa')
+    logging.debug('[SERVER]: Year: %d',  product.year)
+    logging.debug('[SERVER]: Month: %d',  product.month)
+    logging.debug('[SERVER]: CCAA: %s',  product.ccaa)
     db_conn = establish_db_connection()
     region_enocoder, product_encoder, model = get_joblib_models()
     db_conn.autocommit = True
@@ -121,11 +68,15 @@ def get_best_product_for_ccaa(product : OptimalProductCCAA):
     product = dataframe.loc[np.argmax(model.predict(dataframe))]['product']
     best = product_encoder.inverse_transform([int(product)])[0]
     return  {'best_product':best}
-   
 
-@app.get('/specific-product-prediction/')
-def get_specific_product_prediction(product : SpecificProduct):
-    '''Returning returning predicted expenses per capita for given product and month'''
+
+@app.post('/predict-product/')
+def handler_predict_product(product : SpecificProduct):
+    ''' API handler for specific product prediction '''
+    logging.debug('[SERVER]: received request for a specific product prediction')
+    logging.debug('[SERVER]: Year: %d',  product.year)
+    logging.debug('[SERVER]: Month: %d',  product.month)
+    logging.debug('[SERVER]: Product: %s',  product.product)
     db_conn = establish_db_connection()
     _, product_encoder, model = get_joblib_models()
     db_conn.autocommit = True
@@ -138,9 +89,15 @@ def get_specific_product_prediction(product : SpecificProduct):
     value = np.average(model.predict(dataframe))
     return {'value':value}
 
-@app.get('/specific-product-prediction-for-ccaa/')
-def get_specific_product_prediction_for_ccaa(product : SpecificProductCCAA):
-    '''Returning returning predicted expenses per capita for given product, month and region'''
+
+@app.post('/predict-product-by-ccaa/')
+def handler_predict_product_ccaa(product : SpecificProductCCAA):
+    ''' API handler for the specific product prediction for a given CCAA '''
+    logging.debug('[SERVER]: received request for a specific product prediction ccaa')
+    logging.debug('[SERVER]: Year: %d',  product.year)
+    logging.debug('[SERVER]: Month: %d',  product.month)
+    logging.debug('[SERVER]: Product: %s',  product.product)
+    logging.debug('[SERVER]: CCAA: %s',  product.ccaa)
     db_conn = establish_db_connection()
     region_enocoder, product_encoder, model = get_joblib_models()
     db_conn.autocommit = True
@@ -152,3 +109,27 @@ def get_specific_product_prediction_for_ccaa(product : SpecificProductCCAA):
     db_conn.close()
     value = model.predict(dataframe)[0]
     return {'value':value}
+
+
+def get_joblib_models():
+    '''Loading the label encoder for products and regions.
+        Loading the gradient boosting model'''
+    encoder_regions = joblib.load('/home/app/data/encoder_regions.joblib')
+    encoder_products = joblib.load('/home/app/data/encoder_products.joblib')
+    gb_reg = joblib.load('/home/app/data/gradient_boosting_model.joblib')
+
+    return encoder_regions,encoder_products,gb_reg
+
+def establish_db_connection():
+    '''Creating psycopg2 connection'''
+    with open('/home/app/data/credentials.json', 'r', encoding = "utf8") as file:
+        credentials = json.load(file)
+
+    db_conn = psycopg2.connect(
+    database = AGRICULTURE_DATABASE,
+    user = credentials['username'],
+    password = credentials['password'],
+    host = HOST,
+    port = PORT
+    )
+    return db_conn
